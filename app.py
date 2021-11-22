c0402d48d10dea40590c6594c3bb37fdea0525b8
Dev Sapariya <devsapariya94@gmail.com>
2021-11-22 10:48:05 +0530

    .

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from website import create_app
 
 if __name__=="__main__":
           
-          app=create_app()
+        app=create_app()
           
-          app.run(debug=True, port=2000)
+        app.run(debug=True, port=2000)
